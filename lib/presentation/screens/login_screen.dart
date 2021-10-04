198c192d301598141cf762433436942bd9b82b67
Keith Wesley Bacalso <kbacalso@gomigu.com>
2021-10-04 13:42:42 +0800

    Added Google and Facebook sign in.

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:eigital_test/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,6 +15,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
+  final _googleSignIn = GoogleSignIn();
+  final _facebookAuth = FacebookAuth.instance;
   String _email = "", _password = "";
   bool _isLoading = false;
 
@@ -62,10 +66,36 @@ class _LoginScreenState extends State<LoginScreen> {
                           _isLoading = false;
                         });
                       } catch (e) {
-                        print(e);
+                        print("EMAIL LOGIN ERROR: $e");
                       }
                     },
                     child: const Text('LOGIN'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      try {
+                        await _googleSignIn.signIn();
+                        Navigator.of(context).pushNamed(HomeScreen.route);
+                      } catch (e) {
+                        print("GMAIL LOGIN ERROR: $e");
+                      }
+                    },
+                    child: const Text('GMAIL'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      try {
+                        final result = await _facebookAuth.login();
+                        if (result.status == LoginStatus.success) {
+                          Navigator.of(context).pushNamed(HomeScreen.route);
+                        } else {
+                          print("FB AUTH ERROR: ${result.message}");
+                        }
+                      } catch (e) {
+                        print("FACEBOOK LOGIN ERROR: $e");
+                      }
+                    },
+                    child: const Text('FACEBOOK'),
                   ),
                 ],
               ),
